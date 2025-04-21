3e7ccd21d6470f0ac8bdc55384ff305265116b4a
detellis <matthewellis78@gmail.com>
2025-04-21 23:35:56 +0100

    prod deploy

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+    gID: process.env.GID,
+    gSecret: process.env.GSECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY
+};
---
+++ services/passport.js
@@ -0,0 +1,42 @@
+const passport = require('passport');
+const mongoose = require('mongoose');
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) =>{
+    User.findById(id)
+        .then(user => {
+            done(null, user);
+        });
+})
+
+passport.use(new GoogleStrategy(
+    {
+        clientID: keys.gID,
+        clientSecret: keys.gSecret,
+        callbackURL: '/auth/google/callback'    
+    }, 
+    (accessToken, refreshToken, profile, done) => {
+    // console.log('access token', accessToken);
+    // console.log('refresh token', refreshToken);
+    // console.log('profile', profile);
+
+        const user = User.findOne({ googleId: profile.id })
+            .then((existingUser) => {
+                if (existingUser) {
+                    console.log('existingUser', existingUser);
+                    done(null, existingUser);
+                } else {
+                    new User({ googleId: profile.id })
+                        .save()
+                        .then(user => done(null, user));
+                }
+            })
+        }
+));
