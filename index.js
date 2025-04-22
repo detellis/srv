023a22c5694cb61d8daa96a922772b495bf7e44a
detellis <matthewellis78@gmail.com>
2025-04-23 00:35:39 +0100

    routing fix

--- index.js
+++ index.js
@@ -29,14 +29,14 @@ if (process.env.NODE_ENV === 'production' ) {
     app.use(express.static('client/build'));
 
     const path = require('path');
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    app.get(/(.*)/, (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
 }
 
-app.get('/', (req, res) => {
-    res.send({hi: 'there' });
-});
+// app.get('/', (req, res) => {
+//     res.send({hi: 'there' });
+// });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
