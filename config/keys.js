04cdf986ec23a40d163a75f90fc1c7bbf1a14e40
detellis <matthewellis78@gmail.com>
2025-04-22 23:46:24 +0100

    logging

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,7 @@
 if (process.env.NODE_ENV === 'production' ) {
+    console.log("ELLIS", process.env.NODE_ENV);
     module.exports = require('./prod');
 } else {
+    console.log("ELLIS", process.env.NODE_ENV);
     module.exports = require('./dev');
 }
